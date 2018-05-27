b7515397ab5eb4387999b011a2d7550036745bee
Marco Robol <marco89mr@gmail.com>
2018-05-27 23:14:25 +0200

    Intention.toString

--- src/unitn/adk2018/intention/Intention.java
+++ src/unitn/adk2018/intention/Intention.java
@@ -83,7 +83,7 @@ public abstract class Intention<E extends Event> {
 	}
 	
 	public String toString() {
-		return event.getId() + "_" + this.getClass().getSimpleName();
+		return event.getId() + "_" + this.getClass().getSimpleName() + "(" + event + ")";
 	}
 	
 }
